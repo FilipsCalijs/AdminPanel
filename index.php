84777440fb82379f6f76aec971a9d6e69fe89dec
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-06-13 21:13:40 +0300

    add but dont work slim

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
-<!doctype html>
-<html lang="en">
-  <head>
+<?php
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
 
+require_once __DIR__ . '/vendor/autoload.php';
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+$app = AppFactory::create();
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
 
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+$app->addErrorMiddleware(true, true, true);
 
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+})->setName('root');
 
-  </body>
-</html>
+// Run app
+$app->run();
+?> 
