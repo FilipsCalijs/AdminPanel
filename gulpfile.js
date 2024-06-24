bdad82871cfb5898494a527cf07e0788f5eaaa2c
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-06-24 23:39:28 +0300

    add scss and api and conect them

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require("gulp");  // Corrected 'require'
 const webpack = require('webpack-stream');
-
-const dist = "/Users/Filip/Desktop/Programma/PHP/project/admin";
+const sass = require("gulp-sass");
+                // window: B:/promma/xampp/htdocs/react_admin/project/admin
+                // mac: /Users/Filip/Desktop/Programma/PHP/project/admin
+const dist = "B:/promma/xampp/htdocs/react_admin/project/admin";
 gulp.task("copy-html", () => {
     return gulp.src("./app/src/index.html")
         .pipe(gulp.dest(dist));
@@ -35,4 +37,21 @@ gulp.task("build-js", () => {
         }
     }))
     .pipe(gulp.dest(dist))
+});
+
+gulp.task("build-js", () => {
+    return gulp.src("./app/scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(dist))
+});
+
+
+gulp.task("copy-api", () => {
+    return gulp.src("./app/api/**/*.*")
+    .pipe(gulp.dest(dist + "/api"))
+});
+
+gulp.task("copy-assets", () => {
+    return gulp.src("./app/assets/**/*.*")
+    .pipe(gulp.dest(dist + "/assets"))
 });
