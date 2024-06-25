e2aa22bf2c373017a6b023b43e409f9cbc59f387
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-06-25 21:23:12 +0300

    create the first API and working with PHP

--- project/admin/script.js
+++ project/admin/script.js
@@ -10807,7 +10807,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
 
-jquery__WEBPACK_IMPORTED_MODULE_0___default()('h1').fadeOut();
+jquery__WEBPACK_IMPORTED_MODULE_0___default().get("./api", data => {
+  data.forEach(file => {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").append(`<h1>${file}</h1>`);
+  });
+}, "JSON");
 })();
 
 /******/ })()
