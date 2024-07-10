6d92d68406df3f5662cc811d7b9564ddc217c68d
FilipsCalijs <filipscalijs@gmail.com>
2024-07-10 22:24:42 +0300

    Implementation of opening other pages in projects

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -5,7 +5,8 @@ import DOMHelper from '../../helpers/dom-helper';
 import EditorText from '../editor-text';
 import UIkit from 'uikit';
 import Spinner from '../spinner';
-import ConfigModal from "./confirm-modal";
+import ConfirmModal from '../confirm-modal';
+import ChooseModal from '../choose-modal';
 
 export default class Editor extends Component {
     constructor() {
@@ -19,13 +20,19 @@ export default class Editor extends Component {
         this.createNewPage = this.createNewPage.bind(this);
         this.isLoading = this.isLoading.bind(this);
         this.isLoaded = this.isLoaded.bind(this);
+        this.save = this.save.bind(this);
+        this.init = this.init.bind(this);
     }
 
     componentDidMount() {
-        this.init(this.currentPage);
+        this.init(null, this.currentPage);
     }
 
-    init(page) {
+    init(e, page) {
+        if (e) {
+            e.preventDefault();
+        }
+        this.isLoading();
         this.iframe = document.querySelector('iframe');
         this.open(page, this.isLoaded);
         this.loadPageList();
@@ -44,7 +51,8 @@ export default class Editor extends Component {
             })
             .then(DOMHelper.serializeDOMToString)
             .then(html => axios.post("./api/saveTempPage.php", {html}))
-            .then(() => this.iframe.load("../temp.html"))
+            .then(() => this.iframe.load("../yfuy1g221ub_hhg44.html"))
+            .then(() => axios.post("./api/deleteTempPage.php"))
             .then(() => this.enableEditing())
             .then(() => this.injectStyles())
             .then(cb);
@@ -88,7 +96,7 @@ export default class Editor extends Component {
 
     loadPageList() {
         axios
-            .get("./api")
+            .get("./api/pageList.php")
             .then(res => this.setState({pageList: res.data}))
     }
 
@@ -119,7 +127,7 @@ export default class Editor extends Component {
     }
 
     render() {
-        const {loading} = this.state;
+        const {loading, pageList} = this.state;
         const modal = true;
         let spinner;
         
@@ -132,12 +140,12 @@ export default class Editor extends Component {
                 {spinner}
 
                 <div className="panel">
-                    <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-open">открыть</button>
-
+                    <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-open">Открыть</button>
                     <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
                 </div>
                 
-               
+                <ConfirmModal modal={modal}  target={'modal-save'} method={this.save}/>
+                <ChooseModal modal={modal}  target={'modal-open'} data={pageList} redirect={this.init}/>
             </>
         )
     }
